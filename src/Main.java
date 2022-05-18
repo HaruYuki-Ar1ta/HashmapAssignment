0da6de2ee04157e158ac51f727bff03f45fdd810
HaruYuki-Ar1ta <charleCampbell189@gmail.com>
2022-05-18 18:25:39 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> nameGen = new ArrayList<String>();
+        nameGen.add("Alex");
+        nameGen.add("Mick");
+        nameGen.add("Joan");
+
+        HashSet<String> names = new HashSet<String>();
+        for(String i: nameGen){
+            names.add(i);
+            System.out.println(names);
+        }
+        HashMap<String, Integer> FNamesYears = new HashMap<String, Integer>();
+        for(String i : names) {
+
+            FNamesYears.put(i, 2005);
+            System.out.println(FNamesYears);
+
+        }
+
+
+
+
+    }
 }
